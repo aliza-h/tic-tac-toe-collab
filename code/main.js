c92e34d2c48a2736137b053a4473fdfa149b71cd
Aliza Howe <alizahowe26@gmail.com>
2020-11-09 15:24:43 -0500

    Added some JS notes

---
+++ code/main.js
@@ -0,0 +1,16 @@
+// Define all the cells for the JS file. Query selectors (getElementById?), event listeners, etc.
+
+
+// When a cell is clicked, put a symbol in it. We'll worry about the actual symbols later.
+// Have a player's name show up when it's their turn.
+// Don't allow a cell to be clicked twice.
+
+
+// When there are three of the same symbol in a row, column, or diagonal, display a win message and end the game.
+// "Win" conditions:
+	// Group of IDs
+	// Row or column
+
+// "Draw" conditions:
+	// No cells meet the "win" conditions
+// So it's basically a big "if-else" conditional.
