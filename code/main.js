25fbe3c38b9552ad2afb43cc4fe1fa0f5c878039
Aliza Howe <alizahowe26@gmail.com>
2020-11-11 13:44:44 -0500

    Alternates between Xs and Os on game board

--- code/main.js
+++ code/main.js
@@ -1,35 +1,155 @@
-var winnerDeclared = false;//no one will be allowed to place a piece if someone else already won
-var isTurnX = true;
-var turnsLeft = 9;//decremented each time someone places a piece, when equal to zero(all spaces filled)
+let winnerDeclared = false; //no one will be allowed to place a piece if someone else already won
+let isTurnX = true;
+let turnsLeft = 9; //decremented each time someone places a piece, when equal to zero(all spaces filled)
 
-const play = function(){
-  var cell1 = document.getElementById("1");
-  var cell2 = document.getElementById("2");
-  var cell3 = document.getElementById("3");
-  var cell4 = document.getElementById("4");
-  var cell5 = document.getElementById("5");
-  var cell6 = document.getElementById("6");
-  var cell7 = document.getElementById("7");
-  var cell8 = document.getElementById("8");
-  var cell9 = document.getElementById("9");
+const play = function() {
+    let cell1 = document.getElementById("1");
+    let cell2 = document.getElementById("2");
+    let cell3 = document.getElementById("3");
+    let cell4 = document.getElementById("4");
+    let cell5 = document.getElementById("5");
+    let cell6 = document.getElementById("6");
+    let cell7 = document.getElementById("7");
+    let cell8 = document.getElementById("8");
+    let cell9 = document.getElementById("9");
 
 
-  cell1.addEventListener("click",() => {
-    if (!(cell1.textContent == "x" || cell1.textContent == "o"))
-    {
-    if (isTurnX){
-      cell1.textContent = "x";
+    cell1.addEventListener("click", () => {
+        if (!(cell1.textContent == "x" || cell1.textContent == "o")) {
+            if (isTurnX) {
+                cell1.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell1.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
 
-    }
-    else {
-      cell1.textContent = "o";
-    }
-    moves--;
-    isTurnX = !isTurnX;
-  }
+        console.log(isTurnX);
+    });
 
-    console.log(isTurnX);
-  });
+	cell2.addEventListener("click", () => {
+        if (!(cell2.textContent == "x" || cell2.textContent == "o")) {
+            if (isTurnX) {
+                cell2.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell2.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell3.addEventListener("click", () => {
+        if (!(cell3.textContent == "x" || cell3.textContent == "o")) {
+            if (isTurnX) {
+                cell3.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell3.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell4.addEventListener("click", () => {
+        if (!(cell4.textContent == "x" || cell4.textContent == "o")) {
+            if (isTurnX) {
+                cell4.textContent = "x";
+				isTurnX = false;
+
+            } else {
+                cell4.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell5.addEventListener("click", () => {
+        if (!(cell5.textContent == "x" || cell5.textContent == "o")) {
+            if (isTurnX) {
+                cell5.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell5.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell6.addEventListener("click", () => {
+        if (!(cell6.textContent == "x" || cell6.textContent == "o")) {
+            if (isTurnX) {
+                cell6.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell6.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell7.addEventListener("click", () => {
+        if (!(cell7.textContent == "x" || cell7.textContent == "o")) {
+            if (isTurnX) {
+                cell7.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell7.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell8.addEventListener("click", () => {
+        if (!(cell8.textContent == "x" || cell8.textContent == "o")) {
+            if (isTurnX) {
+                cell8.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell8.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
+	cell9.addEventListener("click", () => {
+        if (!(cell9.textContent == "x" || cell9.textContent == "o")) {
+            if (isTurnX) {
+                cell9.textContent = "x";
+				isTurnX = false;
+            } else {
+                cell9.textContent = "o";
+				isTurnX = true;
+            }
+            turnsLeft--;
+        }
+
+        console.log(isTurnX);
+    });
+
 };
 
 play();
@@ -45,9 +165,9 @@ play();
 
 // When there are three of the same symbol in a row, column, or diagonal, display a win message and end the game.
 // "Win" conditions:
-	// Group of IDs
-	// Row or column
+// Group of IDs
+// Row or column
 
 // "Draw" conditions:
-	// No cells meet the "win" conditions
+// No cells meet the "win" conditions
 // So it's basically a big "if-else" conditional.
